6e19a8a50a75b538154f9b78c2dbe89ea4d45bf5
Ubuntu <ubuntu@ip-172-31-31-142.eu-west-1.compute.internal>
2021-09-07 16:20:16 +0000

    finished read and create functionality

--- application/routes.py
+++ application/routes.py
@@ -12,35 +12,45 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/addathlete', methods=['GET', 'POST'])
+@app.route('/add_athlete', methods=['GET', 'POST'])
 def add_athlete():
     form = AddAthlete()
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         athlete_data = Athletes(
             first_name = form.first_name.data,
             last_name = form.last_name.data,
             gender = form.gender.data,
             date_of_birth = form.dob.data,
             country = form.country.data
-        )
-        return 'Form successfully submitted'
+            )
+        db.session.add(athlete_data)
+        db.session.commit()   
+        return redirect(url_for('add_athlete'))
+    
+    return render_template('add_athlete.html',form=form)
 
-    return render_template('addathlete.html',form=form)
-
-@app.route('/addresult', methods=['GET', 'POST'])
+@app.route('/add_result', methods=['GET', 'POST'])
 def add_result():
     form = AddResult()
-    result_data = Results(
-        date = form.date.data,
-        event = form.event.data,
-        medal = form.medal.data,
-        )
-    db.session.add(result_data)
-    db.session.commit
+    if request.method == 'POST' and form.validate_on_submit():
+        result_data = Results(
+            date = form.date.data,
+            event = form.event.data,
+            medal = form.medal.data,
+            )
+        db.session.add(result_data)
+        db.session.commit()
+        return redirect(url_for('add_result'))
 
-    return render_template('addresult.html',form=form)
+    return render_template('add_result.html',form=form)
 
-@app.route('athletes')
+@app.route('/athletes')
 def athletes():
-    athletes = Athletes.query.all()
+    athletesData = Athletes.query.all()
+    return render_template('athletes.html', athletes=athletesData)
+
+@app.route('/results')
+def results():
+    resultsData = results.query.all()
+    return render_template('results.html', results=resultsData)
     
